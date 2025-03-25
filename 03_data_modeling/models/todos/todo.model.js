b32aba0bc723ac9d291039832c2e4048b7152c2b
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-03-25 11:02:35 +0530

    data modeling

---
+++ 03_data_modeling/models/todos/todo.model.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    subTodos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SubTodo',
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Todo = mongoose.model('Todo', todoSchema);
