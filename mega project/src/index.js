a7e950734a2a2e1c76065d7e0a053bbadc5e68f1
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-03-30 14:21:19 +0530

    utils done

--- mega project/src/index.js
+++ mega project/src/index.js
@@ -1,5 +1,21 @@
 // require("dotenv").config({ path: "../env" });
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+const port = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.on("error", () => {
+      console.log("Error: ", error);
+      throw new error(error);
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running at port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connection Failed!! ", error);
+  });
